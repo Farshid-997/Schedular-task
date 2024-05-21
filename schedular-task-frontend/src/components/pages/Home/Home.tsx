ab66224f8e1242f3e219b71d2f90d3040d6a3302
Farshid Ahsan <arnob.farshid@gmail.com>
2024-05-21 21:39:36 +0600

    add

--- schedular-task-frontend/src/components/pages/Home/Home.tsx
+++ schedular-task-frontend/src/components/pages/Home/Home.tsx
@@ -5,22 +5,29 @@ import { NavLink } from "react-router-dom";
 export default function Home() {
   return (
     <div>
-
       <div className="flex justify-center">
+        <h1 className="text-4xl text-center mt-20 font-bold text-blue-600 font-sans">
+          Your Task Application
+        </h1>
 
-      <h1 className="text-4xl text-center mt-20 font-bold text-blue-600 font-sans">Your Task Application</h1>
-
- <div className=" mt-20  text-[#64748b]"><PackageCheck /></div>
-
+        <div className=" mt-20  text-[#64748b]">
+          <PackageCheck />
+        </div>
       </div>
-      
-<div className="flex justify-center mt-24">
-<NavLink to="/add-task"><Button className="mr-10 bg-green-700 hover:bg-green-700 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-100 font-sans" >Create Task </Button></NavLink>   
 
-<NavLink to="/all-task"><Button className=" bg-red-700 hover:bg-red-700 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-100 font-sans">All Task</Button></NavLink>
+      <div className="flex justify-center mt-24">
+        <NavLink to="/add-task">
+          <Button className="mr-10 bg-green-700 hover:bg-green-700 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-100 font-sans">
+            Create Task{" "}
+          </Button>
+        </NavLink>
 
-</div>
-     
+        <NavLink to="/all-task">
+          <Button className=" bg-red-700 hover:bg-red-700 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-100 font-sans">
+            All Task
+          </Button>
+        </NavLink>
+      </div>
     </div>
-  )
+  );
 }
